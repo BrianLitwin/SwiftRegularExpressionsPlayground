4d2fc5cf0d7278007606b756f24d61a192440029
Brian Litwin <brian.n.litwin@gmail.com>
2018-11-05 20:09:11 -0500

    remove for loop

--- RegexPlayground.playground/Contents.swift
+++ RegexPlayground.playground/Contents.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 public extension String {
-    /// An updated implementation of: https://nshipster.com/nsregularexpression/
+    // An updated implementation of: https://nshipster.com/nsregularexpression/
     
-    /// An `NSRange` that represents the full range of the string.
+    // An `NSRange` that represents the full range of the string.
     var nsrange: NSRange {
         return NSRange(location: 0, length: utf16.count)
     }
     
-    /// Returns a substring with the given `NSRange`,
-    /// or `nil` if the range can't be converted.
+    // Returns a substring with the given `NSRange`,
+    // or `nil` if the range can't be converted.
     func substring(with nsrange: NSRange) -> String? {
         guard let range = Range(nsrange, in: self) else { return nil }
         return String(self[range])
@@ -31,13 +31,11 @@ public func test(regex pattern: String, printMatches: Bool = false) {
         let matches = regex.matches(in: line, range: line.nsrange)
         if !matches.isEmpty {
             print("Found match: \"\(line)\"")
-            
-            if printMatches {
-                matches.forEach { match in
-                    for i in 0..<match.numberOfRanges {
-                        if let m = line.substring(with: match.range(at: i)) {
-                            print("match[\(i)]: " + m)
-                        }
+            guard printMatches else { return }
+            matches.forEach { match in
+                for i in 0..<match.numberOfRanges {
+                    if let m = line.substring(with: match.range(at: i)) {
+                        print("match[\(i)]: " + m)
                     }
                 }
             }
@@ -47,7 +45,7 @@ public func test(regex pattern: String, printMatches: Bool = false) {
     print("\n")
 }
 
-// Add lines to test here 
+// Add lines to test here
 lines = [
     ""
 ]
